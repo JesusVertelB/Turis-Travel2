c30346370b9c9e094d9a6eed49306c2b216d014d
JesusVertelB <vertelbravojesus@gmail.com>
2025-12-06 17:26:17 -0500

    Creacion Del proyecto

---
+++ Turis-Travel2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Turis_Travel2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
