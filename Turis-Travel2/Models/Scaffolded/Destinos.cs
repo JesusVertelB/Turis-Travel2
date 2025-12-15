868429075b789b248229a88476181213deb2d518
Jesus <vertelbravojesus@gmail.com>
2025-12-15 17:37:31 -0500

    Destino hecho

---
+++ Turis-Travel2/Models/Scaffolded/Destinos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+
+namespace Turis_Travel2.Models.Scaffolded
+{
+    public class Destinos
+    {
+        public int ID_destinos { get; set; }
+        public string? Nombre_destinos { get; set; }
+        public string? Descripcion { get; set; }
+
+    }
+}
